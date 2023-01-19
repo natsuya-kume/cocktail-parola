2124b4b396c22feace27f5ceb641a1c2376a0e4c
NatsuyaKume <65702108+natsuya-kume@users.noreply.github.com>
2023-01-20 00:55:09 +0900

    ⛏ prettierを追加
    ⛏ eslint修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,54 +1,18 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: 2019,
-    sourceType: "module",
-  },
-  plugins: ["css"],
   extends: [
-    "eslint:recommended",
-    "plugin:@next/next/recommended",
-    "plugin:react/jsx-runtime",
-    "prettier",
-    "plugin:css/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:@typescript-eslint/recommended",
+    'next/core-web-vitals',
+    'plugin:import/recommended',
+    'plugin:import/warnings',
+    'prettier',
   ],
   rules: {
-    // 要調整
-    "@typescript-eslint/naming-convention": 0,
-  },
-  globals: {
-    window: true,
-    localStorage: true,
-    ErrorUtils: false,
-    console: true,
-    alert: true,
-    HTMLDivElement: true,
-    console: true,
-    JSX: true,
-    HTMLInputElement: true,
-  },
-  overrides: [
-    {
-      extends: ["plugin:jest/recommended"],
-      files: ["src/**/*.test.ts{,x}", "src/**/*.spec.ts{,x}"],
-      rules: {
-        "func-names": 0,
-        "jest/no-alias-methods": 0,
-        "jest/valid-title": 0,
-        "jest/no-conditional-expect": 0,
-        "jest/expect-expect": [
-          2,
-          { assertFunctionNames: ["expect", "expectSaga"] },
-        ],
+    'import/order': [
+      'error',
+      {
+        alphabetize: {
+          order: 'asc',
+        },
       },
-    },
-    {
-      files: ["src/**/*.tsx"],
-      rules: {
-        "import/no-default-export": 0,
-      },
-    },
-  ],
-};
+    ],
+  },
+}
