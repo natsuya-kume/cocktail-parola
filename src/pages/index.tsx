b49d13090101835e0e870c784c3809e7fe561f42
NatsuyaKume <65702108+natsuya-kume@users.noreply.github.com>
2023-01-20 23:11:10 +0900

    ⛏ 不要なファイルを削除
    ⛏ テストの画像を取得➡︎表示

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
 import type { NextPage } from 'next'
 import { Button } from '@mui/material'
+import Image from 'next/image'
+import profile from 'public/images/profile.png'
 
 const Home: NextPage = () => {
   return (
     <>
       <Button variant='contained'>Hello World</Button>
+      <Image src={profile} width={200} height={200} alt='テストプロフィール画像' />
     </>
   )
 }
