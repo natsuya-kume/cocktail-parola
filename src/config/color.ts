8c36481c555b3e743734c74f0df93c6120e97c40
NatsuyaKume <65702108+natsuya-kume@users.noreply.github.com>
2023-01-21 21:39:09 +0900

    🎨 スタイル周りのconfigを追加

---
+++ src/config/color.ts
@@ -0,0 +1,15 @@
+import { colors } from '@mui/material'
+
+export const colorConfigs = {
+  sidebar: {
+    bg: '#282a36',
+    hoverBg: '#bd93f9',
+    activeBg: '#ff79c6',
+    fontColor: '#f8f8f2',
+  },
+  topbar: {
+    bg: '#282a36',
+    color: '#000',
+  },
+  mainBg: colors.grey['100'],
+}
---
+++ src/config/theme.ts
@@ -0,0 +1,13 @@
+import { createTheme } from '@mui/material/styles'
+
+export const theme = createTheme({
+  palette: {
+    background: {
+      default: '#282a36',
+      paper: '#383a59',
+    },
+    text: {
+      primary: '#f8f8f2',
+    },
+  },
+})
