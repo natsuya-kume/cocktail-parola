e81882530fc4f064f6f87d559e7002e3acff4ebb
NatsuyaKume <65702108+natsuya-kume@users.noreply.github.com>
2023-01-27 02:25:52 +0900

    fix: フッターのデザインを修正

--- src/components/elements/Footer/Footer.tsx
+++ src/components/elements/Footer/Footer.tsx
@@ -1,7 +1,9 @@
 import { Box, Typography } from '@mui/material'
+import Link from 'next/link'
 import { memo } from 'react'
+import styles from 'src/components/elements/Footer/styles.module.css'
+import { colorConfigs } from 'src/config/color'
 import { theme } from 'src/config/theme'
-
 export const Footer = memo(() => {
   return (
     <Box
@@ -13,9 +15,39 @@ export const Footer = memo(() => {
         width: '100%',
       }}
     >
-      <Typography align='center' sx={{ color: theme.palette.text.primary }}>
-        ©︎ 2023 - Natsuya Kume
-      </Typography>
+      <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '3px' }}>
+        <Typography
+          align='center'
+          variant='h5'
+          sx={{ color: theme.palette.text.primary, fontWeight: 700 }}
+        >
+          Made with
+        </Typography>
+        <span className={styles.movingHeart}>♥</span>
+        <Typography
+          align='center'
+          variant='h5'
+          sx={{ color: colorConfigs.text.primary, fontWeight: 700 }}
+        >
+          by
+        </Typography>
+        <Link href='https://tsunatsuna.vercel.app/'>
+          <Typography
+            align='center'
+            variant='h5'
+            sx={{
+              color: colorConfigs.text.quaternary,
+              fontWeight: 700,
+              display: 'inline-block',
+              '&:hover': {
+                opacity: 0.8,
+              },
+            }}
+          >
+            Natsuya Kume
+          </Typography>
+        </Link>
+      </Box>
     </Box>
   )
 })
