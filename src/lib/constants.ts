65bf06e36eb570d145b594ce6ace61a8d0d9263f
natsuya_kume <natsuya_kume@m-penny.local>
2023-02-27 18:05:38 +0900

    📄 Metaコンポーネントの作成&使用

---
+++ src/lib/constants.ts
@@ -0,0 +1,9 @@
+export const siteMeta = {
+  siteTitle: 'Cocktail Parola',
+  siteDesc: 'カクテル言葉を紹介するサイトです',
+  siteUrl: 'https://cocktail-parola.vercel.app/',
+  siteLang: 'ja',
+  siteLocale: 'ja_JP',
+  siteType: 'website',
+  siteIcon: '/public/favicon.ico',
+}
